5481c79bb37e1acab9781a403e7b25ea15136eee
Alexandrine Dube <adube@c2r1p4.42quebec.com>
2023-10-25 15:49:58 -0400

    exit update

--- mini-work/builtins/exit.c
+++ mini-work/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: adube <adube@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:13:57 by adube             #+#    #+#             */
-/*   Updated: 2023/10/25 15:41:13 by adube            ###   ########.fr       */
+/*   Updated: 2023/10/25 15:49:22 by adube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 bool	ft_exit(t_data data)
 {
 	//ad exit conditions/ error messages
-	ft_lstclear(data->env);
+	ft_lstclear((t_list **)data->env);
 	exit(0);
 }
