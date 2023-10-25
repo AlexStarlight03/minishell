0e8aede905283c45b8ce888232921fef8bde9d0a
Alexandrine Dube <adube@c2r1p4.42quebec.com>
2023-10-25 16:04:15 -0400

    exit new

--- mini-work/builtins/exit.c
+++ mini-work/builtins/exit.c
@@ -6,15 +6,15 @@
 /*   By: adube <adube@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:13:57 by adube             #+#    #+#             */
-/*   Updated: 2023/10/25 15:49:22 by adube            ###   ########.fr       */
+/*   Updated: 2023/10/25 16:00:28 by adube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-bool	ft_exit(t_data data)
+void	ft_exit(t_data data)
 {
 	//ad exit conditions/ error messages
 	ft_lstclear((t_list **)data->env);
-	exit(0);
+	//exit (0);
 }
