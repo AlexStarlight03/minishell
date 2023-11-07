f1e172085323c33694e77e52616490ed3a1e5cc2
Alexandrine Dube <adube@c3r10p2.42quebec.com>
2023-11-07 11:46:51 -0500

    mix maj et min fonctionne

--- mini-work/src/parsing/parse_builtins.c
+++ mini-work/src/parsing/parse_builtins.c
@@ -6,15 +6,33 @@
 /*   By: adube <adube@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 13:31:00 by adube             #+#    #+#             */
-/*   Updated: 2023/11/06 13:52:51 by adube            ###   ########.fr       */
+/*   Updated: 2023/11/07 11:44:09 by adube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include <ctype.h>
 
+char	*make_lower(char *arg)
+{
+	char *newstr;
+	char new;
+	int i;
+
+	i = 0;
+	newstr = malloc(sizeof(char *));
+	while(arg[i] != '\0')
+	{
+		new = ft_tolower(arg[i]);
+		newstr[i++] = new;
+	}
+	newstr[i] = '\0';
+	return (newstr);
+}
+
 int	is_a_builtin(char **args)
 {
+	args[0] = make_lower(args[0]);
 	if (ft_strcmp("cd", args[0]) == 0)
 		return (CD);
 	if (ft_strcmp("echo", args[0]) == 0)
