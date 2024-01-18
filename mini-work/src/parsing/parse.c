040b53c6b20d8e9d5bf680fabe90a13bcf745886
Alexandrine Dube <adube@c3r3p6.42quebec.com>
2024-01-18 14:18:42 -0500

    work on quotes

--- mini-work/src/parsing/parse.c
+++ mini-work/src/parsing/parse.c
@@ -6,36 +6,65 @@
 /*   By: adube <adube@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:47:19 by adube             #+#    #+#             */
-/*   Updated: 2024/01/18 13:15:29 by adube            ###   ########.fr       */
+/*   Updated: 2024/01/18 14:17:28 by adube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-int	quote_check(char quote_type, char *arg)
+bool	quote_check(char quote_type, char *arg)
 {
-	int	nb;
-	int	i;
+	int		len;
+	bool	first;
+	bool	last;
+	int		i;
+	int		index;
+	int		y;
+	char	*new_arg;
 
+	len = strlen(arg);
+	first = false;
+	last = false;
 	i = 0;
-	nb = 0;
+	index = 0;
+	y = 0;
 	while(arg[i++])
 	{
 		if(arg[i] == quote_type)
-			nb += 1;
-	
+		{
+			first = true;
+			break ;
+		}
 	}
-	return (nb);
+	while(arg[len--])
+	{
+		if(arg[len] == quote_type)
+		{
+			last = true;
+			break ;
+		}
+	}
+	if (first == true && last == true)
+	{
+		new_arg = malloc(sizeof(char *));
+		while (arg[index] != '\0')
+		{
+			if (index != i || index != len)
+				new_arg[y] = arg[i];
+			index++;
+		}
+		new_arg[y] = '\0';
+		return (new_arg);
+	}
+	return (arg);
 }
 
-int	analyze(char **args)
+int	analyze(char *args)
 {
-	 int	flag_singlequote;
-	 int	flag_doublequote;
-	 int	i;
-
-	i = 0;
-	flag_singlequote = quote_check( 39, args[i]);
-	flag_doublequote = quote_check( 34, args[i]);
+	 bool	flag_singlequoteduo;
+	 bool	flag_doublequoteduo;
+	
+	flag_singlequoteduo = quote_check(39, args);
+	flag_doublequoteduo = quote_check(34, args);
 	return (0);
 }
 
@@ -43,9 +72,12 @@ void	ft_parse(char *input, t_env *env, t_mini *mini)
 {
 	int		cmd;
 	char	**args;
-
+	int		i;
+	
+	i = 0;
 	args = ft_split(input, ' ');
-	analyze(args);
+	//while (args[i++])
+	analyze(args[i]);
 	cmd = is_a_builtin(args);
 	if (cmd != 1)
 		exec_builtin(mini, env, args, cmd);
